32118aec8140b6a0dbc030d79d65fdb25f69bbf3
Kito <surya.aw26@gmail.com>
2024-04-29 20:35:40 +0700

    first init

--- index.js
+++ index.js
@@ -20,8 +20,31 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api", function (req, res) {
+  let date = new Date()
+
+  res.json({
+    unix: Math.floor(date.getTime() / 1000),
+    utc: date.toUTCString()
+  })
+})
+
+app.get("/api/:date", function (req, res) {
+  const isInvalidDate = (date) => {
+    return date.toUTCString()
+  }
+
+  let date = new Date(req.params.date)
+
+  if (isInvalidDate(date) == 'Invalid Date') {
+    res.json({'error': 'Invalid Date'})
+    return
+  }
+
+  res.json({
+    unix: Math.floor(date.getTime() / 1000),
+    utc: date.toUTCString()
+  })
 });
 
 
